460a5db9798dea03b48ad1a7c429295e93aa9ab9
Het <62541263+HETFADIA@users.noreply.github.com>
2021-02-06 16:32:27 +0530

    Update boiler plate.cpp

--- boiler plate.cpp
+++ boiler plate.cpp
@@ -1,16 +1,12 @@
-// https://repl.it/@hetrakeshfadia/UnfoldedFlickeringGlitch#main.cpp
 #pragma GCC optimize("O2")
 #include <bits/stdc++.h>
 using namespace std;
-#define het                           \
+#define fastio                        \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL);
-#define lld long long int
-#define p(x) cout << x << "\n";
-#define endl "\n"
-#define all(x) x.begin(), x.end()
-#define dll(x) x.begin(), x.end(), greater<lld>()
+#define lld long long
+#define lld long long
 #define p(x) cout << x << "\n";
 #define p2(x, y) cout << x << " " << y << "\n";
 #define p3(x, y, z) cout << x << " " << y << " " << z << "\n";
@@ -22,6 +18,9 @@ using namespace std;
 #define ps4(x, y, z, a) cout << x << " " << y << " " << z << " " << a << " ";
 #define ps5(x, y, z, a, b) cout << x << " " << y << " " << z << " " << a << " " << b << " ";
 #define pns(x) cout << x;
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define dll(x) x.begin(), x.end(), greater<lld>()
 #define vect(x, n) vector<lld> x(n, 0);
 #define matrix(x, n, m) vector<vector<lld>> x(n, vector<lld>(m, 0));
 #define gcd(a, b) __gcd(a, b)
@@ -31,152 +30,10 @@ using namespace std;
 #define pb(x) push_back(x);
 #define pf(x) push_front(x);
 #define mkp(x, y) make_pair(x, y)
-#define f(i,a,b) for(lld i=(a);i<(b);i++)
-#define fr(i,a,b) for(lld i=(b);i>a;i--)
-#define pll pair<lld,lld>
+#define f(i, a, b) for (lld i = (a); i < (b); i++)
+#define fr(i, a, b) for (lld i = (b); i > a; i--)
 #define inf 999999
-void print(vector<lld> &v);
-lld pow(lld x, lld y);
-lld pow(lld x, lld y, lld p);
-void swap(lld &x, lld &y);
-const lld mod = 1e9 + 7;
-lld bintodec(lld n);
-string dectobin(lld n);
-void addedge(unordered_map<lld, set<lld>> &adj, lld u, lld v);
-void dfs(unordered_map<lld, set<lld>> &adj, unordered_map<lld, lld> visited, lld v);
-void bfs(unordered_map<lld, set<lld>> &adj, unordered_map<lld, lld> visited, lld v);
-struct cmpfunc
-{
-    bool operator()(const long &a, const long &b) const
-    {
-        return a > b;
-    }
-};
-// const lld a3=1000000;
-// bool prime[a3 + 1];
-// void SieveOfEratosthenes(int n)
-// {
-
-//     memset(prime, true, sizeof(prime));
-
-//     for (int p = 2; p * p <= n; p++)
-//     {
-//         if (prime[p] == true)
-//         {
-
-//             for (int i = p * p; i <= n; i += p)
-//                 prime[i] = false;
-//         }
-//     }
-// }
-void se()
-{
-}
-int main()
-{
-    het;
-    lld t;
-    cin >> t;
-
-    while (t--)
-    {
-        se();
-    }
-}
-void addedge(unordered_map<lld, set<lld>> &adj, lld u, lld v)
-{
-    adj[u].insert(v);
-    adj[v].insert(u);
-}
-void dfs(unordered_map<lld, set<lld>> &adj, unordered_map<lld, lld> visited, lld v)
-{
-    visited[v] = 1;
-    p(v);
-    for (auto it = adj[v].begin(); it != adj[v].end(); it++)
-    {
-        if (visited[*it] == 0)
-        {
-            dfs(adj, visited, *it);
-        }
-    }
-}
-void bfs(unordered_map<lld, set<lld>> &adj, unordered_map<lld, lld> visited, lld v)
-{
-    visited[v] = 1;
-    queue<lld> q;
-    q.push(v);
-    while (q.empty() == 0)
-    {
-        lld s = q.front();
-        // p(s);
-        q.pop();
-
-        for (auto it = adj[s].begin(); it != adj[s].end(); it++)
-        {
-            if (visited[*it] == 0)
-            {
-                q.push(*it);
-                visited[*it] = 1;
-            }
-        }
-    }
-}
-lld bintodec(lld n)
-{
-    lld num = n;
-    lld dec_value = 0;
-
-    // Initializing base value to 1, i.e 2^0
-    lld base = 1;
-
-    lld temp = num;
-    while (temp)
-    {
-        lld last_digit = temp % 10;
-        temp = temp / 10;
-
-        dec_value += last_digit * base;
-
-        base = base * 2;
-    }
-
-    return dec_value;
-}
-string dectobin(lld n)
-{
-
-    //finding the binary form of the number and
-    //coneverting it to string.
-    string s = bitset<64>(n).to_string();
-
-    //Finding the first occurance of "1"
-    //to strip off the leading zeroes.
-    const auto loc1 = s.find('1');
-
-    if (loc1 != string::npos)
-        return s.substr(loc1);
-
-    return "0";
-}
-void print(vector<lld> &v)
-{
-    for (lld i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
-    }
-}
-lld pow(lld x, lld y)
-{
-    lld res = 1;
-    while (y > 0)
-    {
-        if (y & 1)
-            res = ((res) * (x));
-        y = y >> 1;
-        x = ((x) * (x));
-    }
-    return res;
-}
+#define pll pair<lld, lld>
 lld pow(lld x, lld y, lld p)
 {
     lld res = 1; // Initialize result
@@ -196,4 +53,104 @@ lld pow(lld x, lld y, lld p)
         x = (x * x) % p;
     }
     return res;
+}
+void print(vector<lld> &v)
+{
+    for (lld i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout<<endl;
+}
+// const lld a3=1000000;
+// bool prime[a3 + 1];
+// void SieveOfEratosthenes(int n)
+// {
+
+//     memset(prime, true, sizeof(prime));
+
+//     for (int p = 2; p * p <= n; p++)
+//     {
+//         if (prime[p] == true)
+//         {
+
+//             for (int i = p * p; i <= n; i += p)
+//                 prime[i] = false;
+//         }
+//     }
+// }
+void Counter(vector<lld>&v,unordered_map<lld,lld>&um){
+    for(int i:v){
+        um[i]+=1;
+    }
+}
+unordered_map<lld,lld> Counter(vector<lld>&v){
+    unordered_map<lld,lld>um;
+    for(int i:v){
+        um[i]+=1;
+    }
+    return um;
+}
+void get_list(vector<lld>&v){
+    for(lld i=0;i<v.size();i++){
+        cin>>v[i];
+    }
+}
+string nc="NO";
+string ns="No";
+string yc="YES";
+string ys="Yes";
+void fun()
+{
+    lld n;
+    cin >> n;
+    vector<lld>v(2*n);
+    get_list(v);
+    unordered_map<lld,lld> um;
+    Counter(v,um);
+    for(auto it=um.begin();it!=um.end();it++){
+        if(it->second!=2){
+            p(nc);
+            return;
+        }
+    }
+    sort(v.rbegin(),v.rend());
+    vector<lld>original;
+    lld n_copy=2*n;
+    lld suma=0;
+    for(lld i=0;i<2*n;i+=2){
+        lld element=v[i]-2*suma;
+        if(element%n_copy!=0){
+            p(nc);
+            return;
+        }
+        element=element/n_copy;
+        suma+=element;
+        n_copy-=2;
+        original.pb(element);
+    }
+    for(lld i=0;i<original.size();i++){
+        if(original[i]<=0){
+            p(nc);
+            return;
+        }
+    }
+    um=Counter(original);
+    if(um.size()<original.size()){
+        p(nc);
+        return;
+    }
+    p(yc);
+    return;
+
+}
+int main()
+{
+    fastio;
+    lld t;
+    cin >> t;
+    while (t--)
+    {
+        fun();
+    }
 }
