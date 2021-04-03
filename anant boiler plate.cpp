ad6acfda344a50e19fbb0a3661518e35c85d2567
Het <62541263+HETFADIA@users.noreply.github.com>
2021-04-03 19:57:11 +0530

    Create anant boiler plate.cpp

---
+++ anant boiler plate.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define rep(i,s,e) for(int i=s ; i < e ; i++) // for
+#define rrep(i,s,e) for(int i=s ; i > e ; i--) // reverse for  
+#define srep(i,s,e,j) for(int i=s ; i < e ; i+=j) // step for
+#define tr(i,x) for(auto i : x) // traverse all elements
+#define inp(a) for(int i=0 ; i<a.size() ; i++)cin>>a[i]
+#define int long long
+#define vi vector<int>
+#define vs vector<string>
+#define vb vector<bool>
+#define vpi vector<pii>
+#define maxpqi priority_queue<int>
+#define minpqi priority_queue <int, vector<int>, greater<int> >
+#define pii pair<int,int> 
+#define F first
+#define S second
+#define mk make_pair
+#define pb push_back
+#define pf push_front
+#define endl '\n'
+#define br printf("\n")
+#define inf 1000000
+#define imap map<int,int>   
+#define uimap unordered_map<int,int>
+#define gcd(a,b) __gcd(a,b)
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+#define mod 1000000007
+#define clr(x) memset(x,0,sizeof(x))
+#define fill(x,y) memset(x,y,sizeof(x))
+#define lb lower_bound
+#define ub upper_bound
+#define npos string::npos
+#define all(x) x.begin(),x.end()
+#define descen greater<int>()
